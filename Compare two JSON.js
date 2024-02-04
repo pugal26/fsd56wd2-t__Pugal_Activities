dcafe579383a196dd42bd0fd93177ab205a7bb8f
pugal26 <pugalcud@gmail.com>
2024-02-05 00:39:15 +0530

    task 1 done but doubt

---
+++ Compare two JSON.js
@@ -0,0 +1,8 @@
+let obj1 = { name: "Person 1", age:5 };
+let obj2 = { age:5, name: "Person 1"};
+
+if (obj1 === obj2) {
+    console.log("obj1 and objt2 are same");
+} else{
+    console.log("objt1 and objt2 are different");
+}
---
+++ Display all the country flags.js
@@ -0,0 +1,15 @@
+let XMLHttpRequest = require('xhr2');
+
+let xhr = new XMLHttpRequest();
+
+xhr.open('GET','https://restcountries.com/v3.1/all');
+
+xhr.send();
+
+xhr.onload = function() {
+    let countries = JSON.parse(xhr.responseText);
+
+    for (let data of countries){
+        console.log(data.flags);
+    }
+}
