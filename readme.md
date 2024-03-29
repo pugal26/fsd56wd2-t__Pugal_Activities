d72871284db61b90f3755a91a13b6bec71ea5b77
pugal26 <pugalcud@gmail.com>
2024-03-30 00:18:44 +0530

    readme file updated

---
+++ readme.md
@@ -0,0 +1,30 @@
+# Products Sheet
+
+This project is a simple web application that fetches product data from a fake store API and displays it in a table format. It allows users to view product details such as title, category, description, price, preview image, and rating.
+
+## Features
+
+- Fetches product data from the [Fake Store API](https://fakestoreapi.com/products).
+- Displays product information in a table format.
+- Allows users to hover over product preview images to view an enlarged version.
+- Provides product ratings along with the number of ratings.
+
+## Technologies Used
+
+- HTML
+- CSS (Bootstrap)
+- JavaScript
+
+## How to Use
+
+1. Clone the repository to your local machine:
+
+    ```bash
+    git clone https://github.com/your-username/products-sheet.git
+    ```
+
+
+## Acknowledgements
+
+- Fake Store API for providing the product data.
+- Bootstrap for CSS styling.
--- script.js
+++ script.js
@@ -4,21 +4,21 @@ fetch('https://fakestoreapi.com/products')
         // console.log(products)
 
         let tableData = '';
-        products.map((value) => {
-            tableData +=`<tr>
-                            <td><strong>${value.title}</strong></td>
-                            <td>${value.category[0].toUpperCase()+value.category.slice(1, )}</td>
-                            <td>${value.description}</td>
-                            <td>${value.price}$</td>
-                            <td><img src="${value.image}"/></td>
-                            <td>${value.rating.rate}<br>(${value.rating.count})</td>
-                        </tr>`
+        products.forEach((value,index) => {
+            tableData +=
+                    `<tr>
+                        <td><strong>${value.title}</strong></td>
+                        <td>${value.category[0].toUpperCase()+value.category.slice(1)}</td>
+                        <td>${value.description}</td>
+                        <td>${value.price}$</td>
+                        <td><img src="${value.image}"/></td>
+                        <td>${value.rating.rate}<br>(${value.rating.count})</td>
+                    </tr>`
         });
         let tablebody = document.querySelector('.tablebody')
         tablebody.innerHTML = tableData;
-
     })
-    .catch(error => {
-        alert('Error fetching weather data: ' + error.message);
-    });
+    // .catch(error => {
+    //     alert('Error fetching weather data: ' + error.message);
+    // });
     
